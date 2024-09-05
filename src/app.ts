a8a750c495300eb959b225123e6d387e064fbc84
Pedro Camolesi <potaviocamolesi@gmail.com>
2024-09-05 11:28:25 -0300

    iniciando projeto

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import { DatabaseModel } from './model/DatabaseModel';
+
+//dedinindo a porta do servidor
+const port: number = 3333;
+
+//criando servidor web
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+//rotas da aplicação
+server.get('/', (req, res) => {
+    res.json({ mensagem: "Olá mundo, está é minha primeira aplicação web"});
+});
+
+server.get('/pessoas', (req, res) => {
+    res.json({ mensagem: "Aqui será apresentado os dados das pessoas."});
+});
+
+new DatabaseModel().testeConexao().then((resdb) => {
+    if(resdb) {
+        console.log("Conexão com banco de dados realizada com sucesso!");
+        //iniciando o servidor
+        server.listen(port, () => {
+            console.log(`Servidor iniciado no endereço http://localhost:${port}`);
+        });
+    } else {
+        console.log("Erro ao conectar ao banco de dados!");
+    }
+});
